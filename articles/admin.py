edfeeacb7001eff2c30397adc8ac3978a9d0e27f
abir835 <abirhasan.raj.bd@gmail.com>
2023-08-17 12:53:22 +0600

    comments add article

--- articles/admin.py
+++ articles/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
-from articles.models import Article
+from articles.models import Article, Comment
+
+
 # Register your models here.
 
 
-admin.site.register(Article)
+# class CommentInline(admin.StackedInline):  # new
+#     model = Comment
+#     extra = 0
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+
+class ArticleAdmin(admin.ModelAdmin):  # new
+    inlines = [
+        CommentInline,
+    ]
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
